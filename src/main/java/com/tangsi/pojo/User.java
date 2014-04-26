f756d9f5d8786517baf706d29aed9cdc6821d4c0
tangsi <tangside163@163.com>
2014-04-26 13:53:22 +0800

    authrization,still

--- src/main/java/com/tangsi/pojo/User.java
+++ src/main/java/com/tangsi/pojo/User.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
